8367ba7284cc86da0529b784b62a327db44b20de
pokeboy2512 <haydenbushby@gmail.com>
2025-11-03 01:48:06 +0000

    Update content.js

--- fsparx/content.js
+++ fsparx/content.js
@@ -68,7 +68,7 @@ answerPane.innerHTML = '<strong>Answer:</strong><p id="answerText">No answer yet
 document.body.appendChild(answerPane);
 
 async function queryCohere(question, options, context) {
-    const apiKey = 'KEY-HERE'; // !!!!!~ YOUR API KEY GOES IN THE "KEY-HERE" SECTION !!!!!!
+    const apiKey = 'm3SJRLzwx9zfbOCbA5b6iWR4ZYXQhHdp762zDjis'; // !!!!!~ m3SJRLzwx9zfbOCbA5b6iWR4ZYXQhHdp762zDjis" SECTION !!!!!!
     const response = await fetch('https://api.cohere.ai/generate', {
         method: 'POST',
         headers: {
